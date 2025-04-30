0ac50ff4e76cd68a90f4d849a3d5a84319c4bc56
Luis Miguel Hernández Samaniego <48294999+LuisMHS@users.noreply.github.com>
2025-04-30 13:20:31 -0500

    Update Streaming.ino

--- examples/Streaming/Streaming.ino
+++ examples/Streaming/Streaming.ino
@@ -1,1 +1,47 @@
+//----------------------Header---------------------//
+//Incluir el archivo OV2640.h de la librería ESP32-Camera
+#include <OV2640.h>
+//-------------------------------------------------//
 
+//----------------------Objeto---------------------//
+OV2640 camara;
+//-------------------------------------------------//
+
+//-----------------Constante------------------------//
+//colocar SSID y clave de seguridad de red PASSWORD
+const char *SSID = "Taller robotica";
+const char *PASSWORD = ".*89LuisUnivLM";
+//-------------------------------------------------//
+
+//-----------------Variable------------------------//
+int count = 0;
+String img = " ";
+//-------------------------------------------------//
+
+void setup() {
+  //-------------------------------------------------//
+  //Configurar la comunicación serial a 9600 baudios
+  Serial.begin(115200);
+  //-------------------------------------------------//
+  
+  //-------------Acceso a la red Wi-Fi---------------//
+  camara.begin(SSID, PASSWORD);
+  //-------------------------------------------------//
+
+  //--------------------Streaming--------------------//
+  camara.streaming();
+  //-------------------------------------------------//
+  
+  Serial.print("Camera OV2640! Usa 'http://");
+  Serial.print(camara.ip());
+  Serial.println("' Conectado");
+}
+
+void loop() {
+  //---------------------Photo SD--------------------//
+  count++;
+  img = "casa" + String(count);
+  camara.photo(img.c_str());
+  //-------------------------------------------------//
+  delay(20000);
+}
